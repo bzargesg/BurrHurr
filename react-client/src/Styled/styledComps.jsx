5b40c383b223481ccd0de51b41ab200567510257
bzargesg <bzargesg@gmail.com>
2019-05-06 16:46:03 -0600

    build styled component class

---
+++ react-client/src/Styled/styledComps.jsx
@@ -0,0 +1,47 @@
+import styled from 'styled-components';
+export const ListWrapper = styled.section`
+  color: #4056A1;
+  text-align: left;
+  background-color: #EFE2bA;
+  `;
+  export const SubmissionButton = styled.button`
+    text-align: center;
+    color: #4056A1;
+    position: absolute;
+    font-size: 12px;
+    border-radius: 15px;
+    background-color: #d79922;
+    border: none;
+    outline: none;
+  `;
+  export const ListTable = styled.table`
+    letter-spacing: 0.05em;
+    padding: 5px;
+  `;
+  export const Ltd = styled.td`
+    text-align: center;
+    font-size: 10px;
+  `;
+  export const ListTableHeader = styled.th`
+    padding: 3px;
+    align: center;
+    width: auto;
+  `;
+  export const SubmissionButtonClicked = styled.button`
+  text-align: center;
+  color: #4056A1;
+  align: right;
+  font-size: 12px;
+  border-radius: 15px;
+  background-color: #f7e834;
+  border: none;
+  outline: none;
+`;
+  export const BodyWrapp = styled.section`
+  text-align: center;
+  font-size: 15px;
+  margin: 0;
+  padding: 0;
+  background-color: #EFE2bA;
+  font-family: 'Source Sans Pro', sans-serif;
+ `;
