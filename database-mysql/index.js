fb3e84aa5460e16f1ff802b46149f81e12787f7d
bzargesg <bzargesg@gmail.com>
2019-05-07 09:52:17 -0600

    add helper functions for fermentable interrogation

--- database-mysql/index.js
+++ database-mysql/index.js
@@ -1,11 +1,4 @@
-// var mysql = require('mysql');
 
-// var connection = mysql.createConnection({
-//   host     : 'localhost',
-//   user     : 'student',
-//   password : 'student',
-//   database : 'burrhurr'
-// });
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize('burrhurr', 'student', 'student', {
   host: 'localhost',
@@ -17,14 +10,41 @@ const sequelize = new Sequelize('burrhurr', 'student', 'student', {
     idle: 10000
   }
 });
-var selectAll = function(callback) {
-  connection.query('SELECT * FROM items', function(err, results, fields) {
-    if(err) {
-      callback(err, null);
-    } else {
-      callback(null, results);
-    }
-  });
-};
+sequelize.authenticate()
+.then(()=>{
+  console.log('Connected to database Succesfully');
+})
+.catch(err=>{
+  console.error('Unable to connect to the database:',err)
+});
+const model = require('../models');
 
-module.exports.selectAll = selectAll;
+const fermentables = model.fermentables;
+const recipes = model.recipes;
+
+// var selectAll = function() {
+
+// };
+
+module.exports = {
+  fermentables: {
+    findAll: ()=>{
+      return fermentables.findAll();
+    },
+    add: (fermentable)=>{
+      console.log('add',fermentable)
+      return sequelize.transaction(t => {
+        return fermentables.upsert(fermentable,{transaction: t,fields: 'name'}).then(ferment => {
+          console.log('insertion successfull');
+        })
+      })
+    },
+    delete: (id)=>{
+      return fermentables.destroy({
+        where: {
+          id: id
+        }
+      })
+    }
+  },
+}
