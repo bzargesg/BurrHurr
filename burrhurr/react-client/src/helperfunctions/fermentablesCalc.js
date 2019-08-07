edff4e934011e9f200350fd65c7e0543144d9749
bzargesg <bzargesg@gmail.com>
2019-08-07 14:42:57 -0600

    delete redundant files

--- burrhurr/react-client/src/helperfunctions/fermentablesCalc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  gravityByExtract: (extract)=>{
-    //use fine grind extract
-    //returns gravity contribution in PPG
-      //pound/gallon/gravity unit
-    return 46 * extract;
-  },
-}
